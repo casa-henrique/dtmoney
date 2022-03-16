ba1954876d388335a1d6b90f8a3fdf8706c1de59
casa-henrique <riquecasa@gmail.com>
2022-03-16 13:15:21 -0300

    Estilização e estrutura dos botões do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -19,7 +21,11 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type="button" onClick={onRequestClose} className="react-close-modal">
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-close-modal"
+      >
         <img src={closeImg} alt="Fechar o modal" />
       </button>
 
@@ -28,6 +34,16 @@ export function NewTransactionModal({
 
         <input placeholder="Titulo" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" /> <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saida" /> <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
